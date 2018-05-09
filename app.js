f315e56e82c2282c597cb6b88f47fe8c77a8b355
Matt McQuain <matt.mcquain@gmail.com>
2018-05-08 21:31:40 -0700

    finalized all questions. need to start work on css/html

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if (Hawaii === 'yes' || Hawaii === 'y') {
     userPoints++;
 } else {
     //If user guesses incorrectly, shame them. 
-    alert('I mean, I guess I could be from some other Pacific island...');
+    alert('I mean, I guess I could be from some other Pacific island...but...no');
 }
 
 alert('You have ' + userPoints + ' pineapple points');
@@ -39,26 +39,53 @@ console.log(Surf);
 
 if (Surf === 'yes' || Surf === 'y') {
     //Surely they said yes
-    alert('Right on brah! Another pinapple point for you!')
     userPoints++;
+    alert('Right on brah! Another pinapple point for you! You have ' + userPoints + ' pineapple points')
 } else {
     //If user guesses incorrectly, shame them again. 
-    alert('Yah, I do. I\'m a walking stereo type.');
+    alert('Yah, I do. I\'m a walking stereo type. You have ' + userPoints + ' pineapple points');
 }
 
-alert('You have ' + userPoints + ' pineapple points');
 
-var CodeStatus = prompt('I\'ve only just started learning to code. Have you coded before ' + user + '?', 'type yes or no').toLowerCase();
+var CodeStatus = prompt('I\'ve only just started learning to code. Have you coded before ' + user + '?' , 'type yes or no').toLowerCase();
 
 console.log(CodeStatus);
 
 if (CodeStatus === 'yes' || CodeStatus === 'y') {
     //I'm assuming I am probably the only one that is new to all this
-    alert('Nice! Please don\'t mind all my silly, noob questions')
+    alert('Nice! You get 1 point for having to put up with all my noob questions ')
     userPoints++;
 } else {
    //Is this even a good question? 
     alert('No? Well let\'s do our best to learn together!');
 }
 
-alert('What are you gonna do with ' + userPoints + ' pineapple points!? You can trade them in for hugs');
+alert('What are you gonna do with ' + userPoints + ' pineapple points!? You can trade them in for highfives!');
+
+var Japan = prompt('I\'ve always been really interested in Japan. Have you ever been to Japan ' + user + '?' , 'type yes or no').toLowerCase();
+
+console.log(Japan);
+
+if (Japan === 'yes' || Japan === 'y') {
+    //If you've never been, you really should!
+    userPoints++;
+    alert('Me too! Another point for you for being so cool!')
+} else {
+   //No really, you should go. 
+    alert('No? You really should go! It\'s an amazing country');
+}
+
+var LastQuestion = prompt('Ok, last question ' + user + '. You have ' + userPoints + ' pinapple points. Do you want to be my friend?').toLowerCase();
+
+console.log(LastQuestion);
+
+if (LastQuestion === 'yes' || LastQuestion === 'y') {
+    //I hope they do...
+    userPoints++;
+    alert('Yay! Let\'s be coding buddies!')
+} else {
+   //No really, you should go. 
+    alert('No? Oh, ok then. #Sadface');
+}
+
+alert('You earned a total of ' + userPoints + ' out of 5 points!');
